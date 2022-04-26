3c5756d2f1fa4556003ffef0fa2163ec84e1bcf6
trekitch <trekitch@ymail.com>
2022-04-25 22:01:08 -0400

    added player display and check winner functionality

--- js/script.js
+++ js/script.js
@@ -39,11 +39,19 @@ const game = (() => {
     const playerOne = playerFactory("Player 1", "X");
     const playerTwo = playerFactory("Player 2", "O");
     gameBoard.renderBoard();
-    let turns = 0;
     let obj = playerOne;
+
+    const currentPlayer = document.createElement("p");
+    const playerDisplay = document.querySelector(".diplayPlayer");
+
+    const winner = document.createElement("p");
+    const winnderDisplay = document.querySelector(".displayWinner");
+
+    const gameOver = false;
 
     gameBoard.boardDisplay.addEventListener("click", (e) => {
         if (e.target.textContent) {
+            //prevents double moves
             return;
         } else {
             let squareIndex = e.target.getAttribute("data-index");
@@ -54,12 +62,46 @@ const game = (() => {
 
             //switches between player
             if (obj == playerOne) {
+                gameOver = checkWinner(gameBoard.board, obj);
                 obj = playerTwo;
-                turns++;
+                currentPlayer.textContent = `${obj.name}'s turn`;
+                playerDisplay.appendChild(currentPlayer);
             } else {
+                gameOver = checkWinner(gameBoard.board, obj);
                 obj = playerOne;
-                turns++;
+                currentPlayer.textContent = `${obj.name}'s turn`;
+                playerDisplay.appendChild(currentPlayer);
             }
         }
     });
+
+    function checkWinner(arr, player) {
+        if (arr[0] == player.icon && arr[1] == player.icon && arr[2] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[2] == player.icon && arr[5] == player.icon && arr[8] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[8] == player.icon && arr[7] == player.icon && arr[6] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[6] == player.icon && arr[3] == player.icon && arr[0] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[0] == player.icon && arr[4] == player.icon && arr[8] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[2] == player.icon && arr[4] == player.icon && arr[6] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[1] == player.icon && arr[4] == player.icon && arr[7] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else if (arr[3] == player.icon && arr[4] == player.icon && arr[8] == player.icon) {
+            console.log(`${player.name} wins!!!`);
+            return true;
+        } else {
+            return false;
+        }
+    }
 })();
