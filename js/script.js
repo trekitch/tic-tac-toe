2108843a6d59434cfb7c1084617abe8239356db4
trekitch <trekitch@ymail.com>
2022-04-26 02:45:24 -0400

    Added winner logic and display

--- js/script.js
+++ js/script.js
@@ -44,61 +44,82 @@ const game = (() => {
     const currentPlayer = document.createElement("p");
     const playerDisplay = document.querySelector(".diplayPlayer");
 
-    const winner = document.createElement("p");
-    const winnderDisplay = document.querySelector(".displayWinner");
+    let winner = document.createElement("p");
+    const winnerDisplay = document.querySelector(".displayWinner");
 
-    const gameOver = false;
+    let gameOver = false;
 
     gameBoard.boardDisplay.addEventListener("click", (e) => {
         if (e.target.textContent) {
             //prevents double moves
             return;
-        } else {
+        } else if (gameOver == false) {
             let squareIndex = e.target.getAttribute("data-index");
             console.log(squareIndex);
             e.target.textContent = obj.icon;
             gameBoard.board[squareIndex] = obj.icon;
             console.log(gameBoard.board);
-
-            //switches between player
-            if (obj == playerOne) {
-                gameOver = checkWinner(gameBoard.board, obj);
-                obj = playerTwo;
-                currentPlayer.textContent = `${obj.name}'s turn`;
-                playerDisplay.appendChild(currentPlayer);
-            } else {
-                gameOver = checkWinner(gameBoard.board, obj);
-                obj = playerOne;
-                currentPlayer.textContent = `${obj.name}'s turn`;
-                playerDisplay.appendChild(currentPlayer);
+            if (gameOver == false) {
+                //switches between player
+                if (obj == playerOne) {
+                    gameOver = checkWinner(gameBoard.board, obj);
+                    obj = playerTwo;
+                    currentPlayer.textContent = `${obj.name}'s turn`;
+                    playerDisplay.appendChild(currentPlayer);
+                    return gameOver;
+                } else {
+                    gameOver = checkWinner(gameBoard.board, obj);
+                    obj = playerOne;
+                    currentPlayer.textContent = `${obj.name}'s turn`;
+                    playerDisplay.appendChild(currentPlayer);
+                    return gameOver;
+                }
             }
+        } else {
+            console.log("Game Over");
         }
     });
 
     function checkWinner(arr, player) {
         if (arr[0] == player.icon && arr[1] == player.icon && arr[2] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[2] == player.icon && arr[5] == player.icon && arr[8] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[8] == player.icon && arr[7] == player.icon && arr[6] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[6] == player.icon && arr[3] == player.icon && arr[0] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[0] == player.icon && arr[4] == player.icon && arr[8] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[2] == player.icon && arr[4] == player.icon && arr[6] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[1] == player.icon && arr[4] == player.icon && arr[7] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else if (arr[3] == player.icon && arr[4] == player.icon && arr[8] == player.icon) {
             console.log(`${player.name} wins!!!`);
+            winner.textContent = `${player.name} wins!!!`;
+            winnerDisplay.appendChild(winner);
             return true;
         } else {
             return false;
